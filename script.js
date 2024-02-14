26882a713db49193d025e157de6b835ff31e79b3
Oliver Craigie <ogc@live.ie>
2024-02-14 11:05:05 +0000

    Add variables changing function

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const inputs = document.querySelectorAll(`.controls input`);
 
 function changeInputs() {
-  console.log(inputs);
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
 }
 
 inputs.forEach((input) => input.addEventListener("input", changeInputs));
-inputs.forEach((input) => input.addEventListener("mouseover", changeInputs));
+// inputs.forEach((input) => input.addEventListener("mouseover", changeInputs));
